232ff08eb1a6846ec9386bc335aee7c78753df6e
alk3pInjection <webmaster@raspii.tech>
2023-12-28 15:32:25 +0800

    Reallow local clones with symlinks
    Git fucked up this behavior out of non-sense security concerns.
    
    Due to their kind consideration, users are now constrained to
    use Git transport and enjoy returning to the 33k dial-up era
    while everything is on the local disk.
    
    Revert "Merge branch 'tb/drop-dir-iterator-follow-symlink-bit'"
    
    This reverts commit b8840a72e25176681eaeea88d9a61aa4a02738cf, reversing
    changes made to 63f74cfbcc608fc2b379680ba4f9181792996081.
    
    Revert "builtin/clone.c: disallow `--local` clones with symlinks"
    
    This reverts commit 6f054f9fb3a501c35b55c65e547a244f14c38d56.
    
    Revert "clone: error specifically with --local and symlinked objects"
    
    This reverts commit 4e33535ea98ac16d2163e8e9fcbba5e015881e65.
    
    Revert "dir-iterator: prevent top-level symlinks without FOLLOW_SYMLINKS"
    
    This reverts commit bffc762f87ae8d18c6001bf0044a76004245754c.
    
    Revert "builtin/clone: stop resolving symlinks when copying files"
    
    This reverts commit 150e6b0aedf57d224c3c49038c306477fa159886.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- builtin/clone.c
+++ builtin/clone.c
@@ -249,6 +249,8 @@ static void copy_or_link_directory(struct strbuf *src, struct strbuf *dest,
 	int src_len, dest_len;
 	struct dir_iterator *iter;
 	int iter_status;
+	unsigned int flags;
+	struct strbuf realpath = STRBUF_INIT;
 
 	/*
 	 * Refuse copying directories by default which aren't owned by us. The
@@ -266,20 +268,11 @@ static void copy_or_link_directory(struct strbuf *src, struct strbuf *dest,
 
 	mkdir_if_missing(dest->buf, 0777);
 
-	iter = dir_iterator_begin(src->buf, DIR_ITERATOR_PEDANTIC);
+	flags = DIR_ITERATOR_PEDANTIC | DIR_ITERATOR_FOLLOW_SYMLINKS;
+	iter = dir_iterator_begin(src->buf, flags);
 
-	if (!iter) {
-		if (errno == ENOTDIR) {
-			int saved_errno = errno;
-			struct stat st;
-
-			if (!lstat(src->buf, &st) && S_ISLNK(st.st_mode))
-				die(_("'%s' is a symlink, refusing to clone with --local"),
-				    src->buf);
-			errno = saved_errno;
-		}
+	if (!iter)
 		die_errno(_("failed to start iterator over '%s'"), src->buf);
-	}
 
 	strbuf_addch(src, '/');
 	src_len = src->len;
@@ -291,10 +284,6 @@ static void copy_or_link_directory(struct strbuf *src, struct strbuf *dest,
 		strbuf_addstr(src, iter->relative_path);
 		strbuf_setlen(dest, dest_len);
 		strbuf_addstr(dest, iter->relative_path);
-
-		if (S_ISLNK(iter->st.st_mode))
-			die(_("symlink '%s' exists, refusing to clone with --local"),
-			    iter->relative_path);
 
 		if (S_ISDIR(iter->st.st_mode)) {
 			mkdir_if_missing(dest->buf, 0777);
@@ -310,7 +299,8 @@ static void copy_or_link_directory(struct strbuf *src, struct strbuf *dest,
 		if (unlink(dest->buf) && errno != ENOENT)
 			die_errno(_("failed to unlink '%s'"), dest->buf);
 		if (!option_no_hardlinks) {
-			if (!link(src->buf, dest->buf)) {
+			strbuf_realpath(&realpath, src->buf, 1);
+			if (!link(realpath.buf, dest->buf)) {
 				struct stat st;
 
 				/*
@@ -345,6 +335,7 @@ static void copy_or_link_directory(struct strbuf *src, struct strbuf *dest,
 	}
 
 	dir_iterator_free(iter);
+	strbuf_release(&realpath);
 }
 
 static void clone_local(const char *src_repo, const char *dest_repo)
